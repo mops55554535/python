0ffc0b84aa65547cb136acca51a7dbef47c8096d
mops <banbanbanbanbanban5@gmail.com>
2025-03-04 10:22:03 +0300

    modified:   README.md 	new file:   __pycache__/manage.cpython-311.pyc 	new file:   api/__init__.py 	new file:   api/__pycache__/__init__.cpython-311.pyc 	new file:   api/__pycache__/admin.cpython-311.pyc 	new file:   api/__pycache__/apps.cpython-311.pyc 	new file:   api/__pycache__/models.cpython-311.pyc 	new file:   api/admin.py 	new file:   api/apps.py 	new file:   api/migrations/__init__.py 	new file:   api/migrations/__pycache__/__init__.cpython-311.pyc 	new file:   api/models.py 	new file:   api/tests.py 	new file:   api/views.py 	modified:   base/__pycache__/__init__.cpython-311.pyc 	modified:   base/__pycache__/settings.cpython-311.pyc 	modified:   base/__pycache__/urls.cpython-311.pyc 	modified:   base/__pycache__/wsgi.cpython-311.pyc 	modified:   base/settings.py 	modified:   base/urls.py 	modified:   shop/__pycache__/__init__.cpython-311.pyc 	modified:   shop/__pycache__/admin.cpython-311.pyc 	modified:   shop/__pycache__/apps.cpython-311.pyc 	modified:   shop/__pycache__/models.cpython-311.pyc 	modified:   shop/__pycache__/urls.cpython-311.pyc 	modified:   shop/__pycache__/views.cpython-311.pyc 	modified:   shop/migrations/__pycache__/0001_initial.cpython-311.pyc 	modified:   shop/migrations/__pycache__/__init__.cpython-311.pyc

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 1 - сделать git clone
 
-2 - pipenv install
+2 - pipenv install; py -m pipenv install 
 
-3 - pipenv shell
+3 - pipenv shell; py -m pipenv shell
 
 4 - python manage.py runserver
 
---
+++ api/models.py
@@ -0,0 +1,17 @@
+from tastypie.resources import ModelResource
+from shop.models import Category, Course
+
+
+class CategoryResource(ModelResource):
+    class Meta:
+        queryset = Category.objects.all()
+        resource_name = 'categories'
+        allowed_methods = ['get']
+    
+
+class CourseResource(ModelResource): # CategoryResource(ModelResource) - означает наследование CategoryResource класса ModelResource
+    class Meta:
+        queryset = Course.objects.all()
+        resource_name = 'courses'
+        allowed_methods = ['get', 'post', 'delete']
+        
